4af9205e1df852891c3a68fb3c421eaa1310228a
DESKTOP-CN9GM7H\TUF <godric@hexconiac.com>
2023-03-14 23:07:21 +0900

    프로그래머스 일차 다트 게임 문제 해결

--- src/programmers/일차_다트_게임.java
+++ src/programmers/일차_다트_게임.java
@@ -10,11 +10,12 @@ public class 일차_다트_게임 {
             int prepreScore = 0;
 
             for(int i=1; i<dartResult.length(); i++){
+
                 if(dartResult.charAt(i) == 'S'){
 
                     preScore = toInt(dartResult.charAt(i - 1));
 
-                    if(i-2>0) {
+                    if(i-2>=0) {
                         if(dartResult.charAt(i - 2) == '1')
                             preScore = 10;
                     }
@@ -25,44 +26,34 @@ public class 일차_다트_게임 {
                 }
                 else if(dartResult.charAt(i) == 'D') {
                     preScore = (int) Math.pow(toInt(dartResult.charAt(i - 1)),2);
-                    if(i-2>0) {
+                    if(i-2>=0) {
                         if(dartResult.charAt(i - 2) == '1') preScore = 100;
                     }
                     answer += preScore;
                 }
-                else if(dartResult.charAt(i) == 'D') {
+                else if(dartResult.charAt(i) == 'T') {
                     preScore = (int) Math.pow(toInt(dartResult.charAt(i - 1)),3);
-                    if(i-2>0) {
+                    if(i-2>=0) {
                         if(dartResult.charAt(i - 2) == '1') preScore = 1000;
                     }
                     answer += preScore;
                 }
                 else if(dartResult.charAt(i) == '*') {
-                    answer += preScore;
-
-                    if(i+2 < dartResult.length()) {
-                        if (dartResult.charAt(i + 2) == 'S')
-                            answer += (int) Math.pow(toInt(dartResult.charAt(i + 1)), 1);
-                        else if (dartResult.charAt(i + 2) == 'D')
-                            answer += (int) Math.pow(toInt(dartResult.charAt(i + 1)), 2);
-                        else if (dartResult.charAt(i + 2) == 'T')
-                            answer += (int) Math.pow(toInt(dartResult.charAt(i + 1)), 3);
-                    }
-                    else if(i+3 < dartResult.length()) {
-                        if (dartResult.charAt(i + 3) == 'S') answer += 10;
-                        else if (dartResult.charAt(i + 3) == 'D') answer += 100;
-                        else answer += 1000;
-                    }
+                    answer += (preScore+prepreScore);
+                    preScore *= 2;
+                    prepreScore *= 2;
                 }
                 else if(dartResult.charAt(i) == '#') {
                     answer -= (2*preScore);
+                    preScore = -1*preScore;
                 }
+                prepreScore = preScore;
             }
             return answer;
         }
 
         public int toInt(char n){
-            return Integer.parseInt(String.valueOf(n));
+            return Character.getNumericValue(n);
         }
     }
 
