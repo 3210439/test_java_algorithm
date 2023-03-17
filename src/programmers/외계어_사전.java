b316541ea2f46b834607eb9a4b8c4c7d8b02a640
DESKTOP-CN9GM7H\TUF <godric@hexconiac.com>
2023-03-17 22:45:45 +0900

    프로그래머스 외계어 사전 문제 해결

---
+++ src/programmers/외계어_사전.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class 외계어_사전 {
+
+    class Solution {
+        public static int solution(String[] spell, String[] dic) {
+            int answer = 2;
+
+            for(String d : dic){
+                String temp = "";
+                int count = 0;
+                for(String s : spell){
+                    if(d.contains(s) && !temp.contains(s)){
+                        count++;
+                        temp += s;
+                    }
+                    if(count == spell.length) return 1;
+                }
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+       System.out.println(Solution.solution(new String[]{"z", "d", "x"}, new String[]{"def", "dww", "dzx", "loveaw"}));
+    }
+}
