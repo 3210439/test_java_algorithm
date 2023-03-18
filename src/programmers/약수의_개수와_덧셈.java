32baf0af1ebcbdceeb8c0ce1cb26e44b9b17fd3e
kimJongWan <kimdeiz@naver.com>
2023-03-18 21:41:35 +0900

    프로그래머스 약수의 개수와 덧셈 해결

---
+++ src/programmers/약수의_개수와_덧셈.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class 약수의_개수와_덧셈 {
+
+
+    class Solution {
+        public int solution(int left, int right){
+            int answer = 0;
+
+            for(int i=left; i<right+1; i++){
+                int count = 0;
+                for(int j=1; j<i+1; j++){
+                    if(i%j == 0) count ++;
+                }
+                if(count % 2 == 0) answer += i;
+                else answer -= i;
+            }
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
