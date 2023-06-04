1f39b7e8bffff3ab51ac38ab6e872c843b9038cb
kimJongWan <kimdeiz@naver.com>
2023-06-04 21:15:45 +0900

    프로그래머스 카펫 문제 해결

--- src/programmers/level2/dfsbfs/카펫.java
+++ src/programmers/level2/dfsbfs/카펫.java
@@ -1,34 +1,71 @@
 package programmers.level2.dfsbfs;
 
-public class 카펫 {
-    // 1. 테두리 1줄은 갈색이다.
-    // 2. 사각형 넓이가 주어진다.
-    // 3. 소수를 구한다.
+import java.util.ArrayList;
+import java.util.List;
 
-    static class Solutnion {
+public class 카펫 {
+    /**
+     * brown + yellow 는 사각형의 넓이이다.
+     * return 값들은 사각형의 넓이의 약수 값이다.
+     * 사각형 넓이 값의 약수 2개의 조합 중에서 곱할 때 사각형 넓이와 같은 것들의 조합 중에 답이 있다.
+     */
+
+    // 48 의 약수 중 가능한 조합 (16, 3), (12, 4), (8, 6)
+    // brown 24 yellow 24
+    // 여기서 사각형 크기는 테두리의 길이는 24+4인 28
+    // 2 * (8 + 6) = 24이므로 8 과 6의 조합으로 구성된다는 것을 알 수 있다.
+
+    static class Solution {
 
         public int[] solution(int brown, int yellow){
             int[] answer = {};
             int total = brown + yellow;
-            int totalSq = (int) Math.sqrt(total);
+            List<Integer> measures = new ArrayList<>();
+            int totalSq = (int)Math.sqrt(total);
 
-            // ex 3
-            for(int i = 1; i <= totalSq; ++i){
-                if( totalSq % i == 0){
-                    answer = new int[]{i,totalSq / i};
-                    break;
+            for(int i=3; i<=totalSq; i++){
+                // total % i 는 공약수를 나타내는 조건
+                if(total % i == 0){
+                    measures.add(i);
+
+                    // 이미 추가된 값은 무시한다.
+                    if(total/i != i){
+                        measures.add(total/i);
+                    }
+
+
+                    if(i*i == total){
+                        measures.add(i);
+                    }
                 }
             }
 
+
+            for(int i=0; i<measures.size()-1; i++){
+                for(int j=i+1; j<measures.size(); j++){
+                    int width = Math.max(measures.get(i), measures.get(j));
+                    int height = Math.min(measures.get(i), measures.get(j));
+                    int area = width * height;
+
+                    if(area == total){
+                        int squareSize = brown + 4;
+                        int controlSquareSize = 2*(width + height);
+                        if(squareSize == controlSquareSize){
+                            answer = new int[] {width, height};
+                            break;
+                        }
+                    }
+                }
+            }
 
             return answer;
         }
     }
 
     public static void main(String[] args) {
-        Solutnion solutnion = new Solutnion();
+        Solution solution1 = new Solution();
 
-        int[] solution = solutnion.solution(10, 2);
+        int[] solution = solution1.solution(24, 24);
 
         for( int a : solution){
             System.out.println(a);
